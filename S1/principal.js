962516b4cc320b8ba4c7f4a9d91ba29039ee8bbf
Andres Ramirez <andres200493@gmail.com>
2022-10-18 20:47:11 -0500

    S1 - R0

--- S1/principal.js
+++ S1/principal.js
@@ -4,4 +4,21 @@ function sumarFn(num1, num2) {
 
 // sumar = (num1, num2) => console.log(num1 + num2)
 
-module.exports = sumarFn
+function restarFn(num1, num2) {
+  console.log(num1 - num2)
+}
+
+function multiplicarFn(num1, num2) {
+  console.log(num1 * num2)
+}
+
+function dividirFn(num1, num2) {
+  console.log(num1 / num2)
+}
+
+module.exports = {
+  sumarFn,
+  restarFn,
+  multiplicarFn,
+  dividirFn
+}
--- S1/server.js
+++ S1/server.js
@@ -1,3 +1,15 @@
-let sumarFn = require('./principal')
+// let principal = require('./principal')
+
+// principal.sumarFn(5, 7)
+
+let {
+  sumarFn,
+  restarFn,
+  multiplicarFn,
+  dividirFn
+} = require('./principal')
 
 sumarFn(5, 7)
+restarFn(5, 7)
+multiplicarFn(5, 7)
+dividirFn(5, 7)
