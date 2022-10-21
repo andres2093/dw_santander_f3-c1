3866eb095f8a21e04ed219de6ba6b4505bdd8519
Andres Ramirez <andres200493@gmail.com>
2022-10-20 21:53:14 -0500

    S2 - R3

--- S2/e3.js
+++ S2/e3.js
@@ -1,4 +1,11 @@
 const https = require('https')
+const path = require('path')
+const fs = require('fs')
+
+descargarImagen = (url, imagenName) => {
+  let file = fs.createWriteStream(path.join(__dirname, imagenName + ".png"))
+  https.get(url, res => res.pipe(file))
+}
 
 function getAPOD() {
   https.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY', (resp) => {
@@ -9,10 +16,16 @@ function getAPOD() {
     })
 
     resp.on('end', () => {
-      let body = JSON.parse(data)
-      console.log(body.title)
-      console.log(body.explanation)
-      console.log(body.url)
+      try {
+        let body = JSON.parse(data)
+        console.log(body.title)
+        console.log(body.explanation)
+        console.log(body.url)
+
+        descargarImagen(body.url, body.title)
+      } catch (error) {
+        console.log(error)
+      }
     }).on("Error", (err) => {
       console.log(err.message)
     })
