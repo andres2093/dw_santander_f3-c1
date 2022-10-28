2e736db9fe7c207e18657560213beba19d280a3b
Andres Ramirez <andres200493@gmail.com>
2022-10-27 20:56:19 -0500

    S4 - R2

--- S4/server.js
+++ S4/server.js
@@ -1,9 +1,17 @@
 const express = require('express')
+const fs = require('fs')
+const path = require('path')
 
 const app = express()
+app.use(express.json())
 
 app.get('/', (request, response) => {
-  response.send('Hola mundo!!!')
+  // response.setHeader('Content-Type', 'text/plain')
+  response.send(
+    fs.readFileSync(
+      path.join(__dirname + '/index.html'), { encoding: 'utf-8' }
+    )
+  )
 })
 
 app.listen(8080)
