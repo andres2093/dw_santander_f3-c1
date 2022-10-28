8c6bd4c7e38b449b6714e86a257b64ff72deea0a
Andres Ramirez <andres200493@gmail.com>
2022-10-27 21:43:57 -0500

    S4 - R3

--- S4/server.js
+++ S4/server.js
@@ -5,17 +5,32 @@ const path = require('path')
 const app = express()
 app.use(express.json())
 
-const notes = [
+let notes = [
   {
     id: 1,
     title: 'Dummy',
     content: 'Nota dummy'
   }
 ]
-
+// GET -> Obtener notas
 app.get('/notes', (request, response) => {
-  response.send(notes)
+  response.send(notes) 
   // response.json(notes)
+})
+// POST -> Añadir nota
+app.post('/notes', (request,response) => {
+  notes.push(request.body)
+  response.send(`Nota ${request.body.id} agregada`)
+})
+// PATCH -> Editar nota
+app.patch('/notes', (request,response) => {
+  notes[request.body.id - 1] = request.body
+  response.send(`Nota ${request.body.id} modificada`)
+})
+// DELETE -> Eliminar nota
+app.delete('/notes', (request,response) => {
+  notes = notes.filter(note => note.id !== request.body.id)
+  response.send(`Nota ${request.body.id} eliminada`)
 })
 
 app.get('/', (request, response) => {
