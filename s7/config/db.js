2882650a0a89f305b3c083a33f9cb1bc931f7172
Andres Ramirez <andres200493@gmail.com>
2022-11-08 21:30:06 -0600

    S7 - Relaciones

--- s7/config/db.js
+++ s7/config/db.js
@@ -3,6 +3,7 @@ const { Sequelize } = require('sequelize')
 const productModel = require('../models/product')
 const reviewModel = require('../models/review')
 const userModel = require('../models/user')
+const orderModel = require('../models/order')
 
 const sequelize = new Sequelize(
   'sesion6',
@@ -16,13 +17,15 @@ const sequelize = new Sequelize(
   }
 )
 
-const models = [ productModel, reviewModel, userModel ]
+const models = [ productModel, reviewModel, userModel, orderModel ]
 
 for(let model of models)
   model(sequelize)
 
 // Configuring relations
-const { products, reviews } = sequelize.models;
+const { products, reviews, users, orders } = sequelize.models;
 reviews.belongsTo(products); // Relation one-to-one in reviews table
+orders.belongsTo(users); // Relation: Order has one user
+orders.belongsTo(products); // Relation: Order has one product
 
 module.exports = sequelize
--- s7/routes/index.js
+++ s7/routes/index.js
@@ -5,5 +5,6 @@ router.use('/auth', require('./auth'))
 router.use('/users', require('./user'))
 router.use('/products', require('./products'))
 router.use('/reviews', require('./reviews'))
+router.use('/orders', require('./orders'))
 
 module.exports = router
