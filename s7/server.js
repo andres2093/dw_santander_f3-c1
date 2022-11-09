908d6b1075215b8d010e960b26dd37f474353152
Andres Ramirez <andres200493@gmail.com>
2022-11-08 19:27:47 -0600

    S7 - E1

--- s7/server.js
+++ s7/server.js
@@ -1,6 +1,12 @@
 const express = require('express')
+const helmet = require('helmet')
+const cors = require('cors')
 
 const app = express()
+
+app.use(helmet())
+app.use(cors())
+
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
